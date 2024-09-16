50528a52eb82204d6b7a186e724ae810b128dab8
Greta <tangreta98@gmail.com>
2024-09-16 23:35:54 +0300

    data structures

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,22 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+void reverse_listint(listint_t **head);
+int is_palindrome(listint_t **head);
+void print_python_list_info(PyObject *p)
